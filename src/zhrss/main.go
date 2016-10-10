4efd086f2bd210f22aab73153473dfb061b10fcf
MrLYC <imyikong@gmail.com>
2016-10-10 13:38:46 +0800

    pass time zone from command line

--- src/zhrss/main.go
+++ src/zhrss/main.go
@@ -20,12 +20,13 @@ type resultInfo struct {
 	Result      string
 }
 
-type zhihuInfo struct {
+type sysInfo struct {
 	resultInfo
-	Link string
+	Link     string
+	Location *time.Location
 }
 
-func (i *zhihuInfo) fetchDoc() (*goquery.Document, error) {
+func (i *sysInfo) fetchDoc() (*goquery.Document, error) {
 	doc, err := goquery.NewDocument(i.Link)
 	if err != nil {
 		return nil, err
@@ -34,13 +35,13 @@ func (i *zhihuInfo) fetchDoc() (*goquery.Document, error) {
 	return doc, nil
 }
 
-func (i *zhihuInfo) parseFeed(doc *goquery.Document) *feeds.Feed {
+func (info *sysInfo) parseFeed(doc *goquery.Document) *feeds.Feed {
 	itemsQ := doc.Find(
 		"#zh-profile-activity-page-list .zm-profile-section-item",
 	)
 	items := make([]*feeds.Item, 0, itemsQ.Length())
 	var item *goquery.Selection
-	linkInfo, err := url.Parse(i.Link)
+	linkInfo, err := url.Parse(info.Link)
 	if err != nil {
 		log.Print("parse feed failed due to: ", err)
 		return nil
@@ -56,7 +57,7 @@ func (i *zhihuInfo) parseFeed(doc *goquery.Document) *feeds.Feed {
 		if err != nil {
 			continue
 		}
-		created := time.Unix(timestamp, 0).UTC()
+		created := time.Unix(timestamp, 0).In(info.Location)
 		linkQ := item.Find(".zm-profile-section-main a").Last()
 
 		link, successed := linkQ.Attr("href")
@@ -86,59 +87,60 @@ func (i *zhihuInfo) parseFeed(doc *goquery.Document) *feeds.Feed {
 
 	return &feeds.Feed{
 		Title: doc.Find("title").First().Text(),
-		Link:  &feeds.Link{Href: i.Link},
+		Link:  &feeds.Link{Href: info.Link},
 		Description: doc.Find(
 			"div.zm-profile-header-description span.content",
 		).First().Text(),
 		Author: &feeds.Author{Name: doc.Find(
 			"div.title-section span.name",
 		).First().Text()},
-		Created: time.Now().UTC(),
+		Created: time.Now().In(info.Location),
 		Items:   items,
 	}
 }
 
-func (i *zhihuInfo) handle(w http.ResponseWriter, r *http.Request) {
+func (info *sysInfo) handle(w http.ResponseWriter, r *http.Request) {
 
-	now := time.Now().UTC()
-	if now.Before(i.NextCheckAt) {
+	now := time.Now().In(info.Location)
+	if now.Before(info.NextCheckAt) {
 		log.Print("return result from cache")
-		fmt.Fprint(w, i.Result)
+		fmt.Fprint(w, info.Result)
 		return
 	}
 	log.Print("ready to refresh result")
 
-	doc, err := i.fetchDoc()
+	doc, err := info.fetchDoc()
 	if err != nil {
 		log.Print("return result from cache due to: ", err)
-		fmt.Fprint(w, i.Result)
+		fmt.Fprint(w, info.Result)
 		return
 	}
-	feed := i.parseFeed(doc)
+	feed := info.parseFeed(doc)
 	if feed == nil {
 		log.Print("return result from cache due to empty feed")
-		fmt.Fprint(w, i.Result)
+		fmt.Fprint(w, info.Result)
 		return
 	}
 
 	rss, err := feed.ToRss()
 	if err != nil {
 		log.Print("return result from cache due to: ", err)
-		fmt.Fprint(w, i.Result)
+		fmt.Fprint(w, info.Result)
 		return
 	}
-	if i.NextCheckAt.Before(now) {
-		i.Result = rss
-		i.NextCheckAt = now.Add(time.Duration(i.CacheTTL) * time.Second)
-		log.Print("next check at ", i.NextCheckAt)
+	if info.NextCheckAt.Before(now) {
+		info.Result = rss
+		info.NextCheckAt = now.Add(time.Duration(info.CacheTTL) * time.Second)
+		log.Print("next check at ", info.NextCheckAt)
 	}
 
-	fmt.Fprint(w, i.Result)
+	fmt.Fprint(w, info.Result)
 }
 
 func main() {
 	var serverAddr string
-	info := new(zhihuInfo)
+	var locationName string
+	info := new(sysInfo)
 	info.NextCheckAt = time.Now().UTC()
 
 	flag.StringVar(
@@ -146,12 +148,19 @@ func main() {
 		"user time line page url",
 	)
 	flag.StringVar(&serverAddr, "addr", ":8080", "address to listen")
+	flag.StringVar(&locationName, "location", "utc", "location name")
 	flag.Int64Var(&info.CacheTTL, "cache", 600, "result cache ttl")
 	flag.Parse()
 
+	location, err := time.LoadLocation(locationName)
+	if err != nil {
+		log.Fatal("load location failed: ", err)
+	}
+	info.Location = location
+
 	http.HandleFunc("/", info.handle)
 	log.Printf("feed server for %s listen on %s", info.Link, serverAddr)
-	err := http.ListenAndServe(serverAddr, nil)
+	err = http.ListenAndServe(serverAddr, nil)
 	if err != nil {
 		log.Fatal("server start failed: ", err)
 	}
