b33b97434d51abad88e3da64e0dfcd789db48d21
MrLYC <imyikong@gmail.com>
2016-10-13 21:45:21 +0800

    fix Dockerfile

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/ZHRSS/bin/zhrss -url=${URL} -addr=${ADDR} -location=${LOCATION} -cache=${CACHE} -path=${PATH}
+/app/ZHRSS/bin/zhrss -url=${URL} -location=${LOCATION} -cache=${CACHE} -path=${PATH} -addr=${ADDR}
